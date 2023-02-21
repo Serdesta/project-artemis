fdddb963269d0f8c459ca438e05961b73c3a43f5
Serdesta <Sidar.kaya@gmail.com>
2023-02-21 14:31:37 -0600

    modified main.tf

--- modules/terraform-helm/main.tf
+++ modules/terraform-helm/main.tf
@@ -1,10 +1,27 @@
-resource "artemis-terraform-helm" {
+module "artemis" {
   source    = "./modules/terraform-helm"
   name      = "artemis"
   namespace = "artemis"
   chart     = "application"
   wait      = false
-  values = [
-    var.values_yaml
-  ]
+  values = <<EOF
+controller:
+  image: "docker.awssidar.com/artemis
+  tag: "2.0.0"
+  
+  ingress:
+  enabled: true
+  className: "nginx"
+  annotations: 
+    cert-manager.io/cluster-issuer: letsencrypt-prod
+  hosts:
+    - host: application.awssidar.com
+      paths:
+        - path: /
+          pathType: ImplementationSpecific
+  tls: 
+   - secretName: artemis
+     hosts:
+       - "application.awssidar.com"
+EOF
 }
